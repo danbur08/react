ead68f10d22305391acb14a8bbae10b66ad8ee10
danbur08 <95135771+danbur08@users.noreply.github.com>
2022-07-22 13:25:02 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <header className = 'header'>
+     <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Open_Broadcaster_Software_Logo.png/2048px-Open_Broadcaster_Software_Logo.png"/>
+    </header>
+  );
+};
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,9 @@
+function Profile() {
+  return (
+    <div className='content'><img className="img" src = 'https://overcoder.net/img/1/2/99/19825.png'/>
+    <div>account</div>
+    <div>post</div>
+    </div>
+  );
+};
+export default Profile
